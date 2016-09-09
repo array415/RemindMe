fd54af96192c5519e1c9acc076ed3ae8841afe0b
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-09-09 15:07:52 -0700

    Basic User Crud ready for Authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    end
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :phone_num, :password_digest)
+  end
 end
