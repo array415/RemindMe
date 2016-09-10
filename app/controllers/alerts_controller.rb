2d4c9fb74c3c73aa872c4ad728b29451f46e63b0
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-09-10 11:43:19 -0700

    med create complete

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,2 +1,20 @@
 class AlertsController < ApplicationController
+
+  def new
+    @alert = Alert.new
+  end
+
+  def create
+    @alert = Alert.new(alert_params)
+    if @alert.save
+      redirect_to '/'
+    end
+
+  end
+
+  private
+
+  def alert_params
+    alert_params.require(:alert).permit(:time)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
 
+  get '/meds/new', to: 'meds#new', as: 'new_med'
+  post '/meds', to: 'meds#create'
+
+  get '/alerts/new', to: 'alerts#new', as: 'new_alert'
+  post '/alerts', to: 'alerts#create'
+
 end
