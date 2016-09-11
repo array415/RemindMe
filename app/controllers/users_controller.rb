952238167147f2fb9bf95e8ef1e1fae0746d43de
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-09-11 10:44:33 -0700

    user show basic authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+    if current_user != @user
+      redirect_to '/'
+    end
   end
 
   def new
