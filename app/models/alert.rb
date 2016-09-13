38d318fd57c1809196ba201bdec532b90c8835bd
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-09-13 07:05:32 -0700

    added twilio to alert model

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,4 +1,20 @@
 class Alert < ApplicationRecord
   belongs_to :user
   belongs_to :med
+
+  @@REMINDER_TIME = 5.minutes
+
+  def send_text
+    @client = Twilio::REST::Client.new ENV[twilio_sid], ENV[twilio_token]
+    @message = @client.account.sms.messages.create(
+       from: ENV[twilio_number],
+       to: "+14153610872",
+       body: 'This is a test for RemindMe'
+    )
+  end
+
+  def when_to_run
+    alert_time - @@REMINDER_TIME
+  end
+
 end
