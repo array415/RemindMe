95bd1a0d9af4fad7aa930895359f9361ae05ae46
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-09-13 10:26:14 -0700

    twilio sends texts

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,19 +2,21 @@ class Alert < ApplicationRecord
   belongs_to :user
   belongs_to :med
 
-  @@REMINDER_TIME = 5.minutes
+  after_create :send_text
 
   def send_text
-    @client = Twilio::REST::Client.new ENV[twilio_sid], ENV[twilio_token]
-    @message = @client.account.sms.messages.create(
-       from: ENV[twilio_number],
-       to: "+14153610872",
-       body: 'This is a test for RemindMe'
+    @client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
+    message = @client.account.sms.messages.create(
+       :from => ENV["TWILIO_NUMBER"],
+       :to => "+14153610872",
+       :body => "Hey, #{user.name}! This is a reminder from RemindMe to remind you to take #{med.med_name}"
     )
+    puts message.to
   end
 
   def when_to_run
-    alert_time - @@REMINDER_TIME
+    alert_time
   end
 
+  handle_asynchronously :send_text, :run_at => Proc.new {|i| i.when_to_run}
 end
